22b558d2862c9bb8fafc5ccb22ba58d02e26e6ef
abhijith <kini2u@gmail.com>
2022-11-03 21:24:57 +0530

    Remove adjacent duplicates using stack

--- src/FindSquareRoot.java
+++ src/FindSquareRoot.java
@@ -6,7 +6,7 @@ public class FindSquareRoot {
 
     public static void main(String[] args) {
 
-        int num = 25;
+        int num = 27;
 
         int l = 0;
         int h = num / 2;
@@ -14,7 +14,6 @@ public class FindSquareRoot {
 
         while (l <= h) {
             int mid = (l+h) / 2;
-            System.out.println(mid);
 
             if (mid * mid == num) {
                 ans = mid;
@@ -25,9 +24,12 @@ public class FindSquareRoot {
             if (mid * mid < num) {
                 l = mid + 1;
                 ans = mid;
+
             } else {
                 h = mid - 1;
             }
+
+
         }
         System.out.println("Square of " + num + " is : " + ans);
 
---
+++ src/RemoveAdjDupStack.java
@@ -0,0 +1,42 @@
+/*
+Remove adjacent duplicate using
+ */
+
+import java.util.Stack;
+
+public class RemoveAdjDupStack {
+
+    public static void main (String [] args){
+
+        String str = "mississipie";
+
+        Stack<Character> stack = new Stack<>();
+
+        int i = 0;
+
+        while (i < str.length()){
+
+            char c = str.charAt(i);
+
+            if(!stack.isEmpty() && stack.peek() == c){
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+
+            i++;
+
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        while (!stack.isEmpty()){
+            res.append(stack.pop());
+        }
+
+        System.out.println("Ans : "+ res.reverse().toString());
+
+
+
+    }
+}
